4f62a23d5ed85de5d4f43149381dec91ec214051
Mari Becker <mary12_becker@hotmail.com>
2020-07-08 15:47:45 -0300

    Conta Poupança

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,12 +2,6 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
   static numeroDeContas = 0;
-  //atributo público
-  agencia;
-  //atributo privado
-  _cliente;
-  //atributo privado
-  _saldo = 0;
 
   set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
@@ -26,6 +20,7 @@ export class ContaCorrente {
   constructor(agencia, cliente) {
     this.agencia = agencia;
     this.cliente = cliente;
+    this._saldo = 0;
     ContaCorrente.numeroDeContas += 1;
   }
 
