00f2984f468bdfbeff6243c175396220db12dc54
Mari Becker <mary12_becker@hotmail.com>
2020-07-11 16:16:31 -0300

    Sobreescrevendo o comportamento de sacar

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -8,12 +8,10 @@ export class ContaCorrente extends Conta {
     ContaCorrente.numeroDeContas += 1;
   }
 
+  //Sobreescrevendo o comportamento de sacar
+
   sacar(valor) {
     let taxa = 1.1;
-    const valorSacado = taxa * valor;
-    if (this._saldo >= valorSacado) {
-      this._saldo -= valorSacado;
-      return valorSacado;
-    }
+    return this._sacar(valor, taxa);
   }
 }
