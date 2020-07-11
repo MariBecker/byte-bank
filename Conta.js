e0fd37955bebed251b6eec1a436a25ba380a21a4
Mari Becker <mary12_becker@hotmail.com>
2020-07-11 16:42:52 -0300

    Throw new error

--- Conta.js
+++ Conta.js
@@ -1,11 +1,13 @@
+//Classe abstrata = só funciona para ser herdada
+
 export class Conta {
   constructor(saldoInicial, cliente, agencia) {
+    if (this.constructor == Conta) {
+      throw new Error("Você não deveria instanciar um objeto do tipo conta diretamente");
+    }
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
-    if (this.constructor == Conta) {
-      console.log("Você não deveria instanciar um objeto do tipo conta");
-    }
   }
 
   set cliente(novoValor) {
