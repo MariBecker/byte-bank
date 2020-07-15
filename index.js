11143b5a5e5483eca8ca8143f7a7823f369a5d63
Mari Becker <mary12_becker@hotmail.com>
2020-07-15 18:13:08 -0300

    Interfaces

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Rodrigo", 5000, 11234567871);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123");
+const cliente = new Cliente("Lais", 743936759, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-console.log(estaLogado);
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
+
